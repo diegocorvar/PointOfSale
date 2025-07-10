c604f0b8ed41f1335eec3d97b528f283c05c70e6
Diego Alonso Coronel Vargas <diegocoronel@outlook.com>
2025-07-09 20:01:14 -0600

    connection to the database

--- Forms/LoginFrm.cs
+++ Forms/LoginFrm.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SQLite;
+using PuntoDeVenta.Helpers;
+
 namespace PuntoDeVenta.Forms
 {
     public partial class LoginFrm: Form
@@ -31,15 +34,34 @@ namespace PuntoDeVenta.Forms
             user = txtUser.Text;
             password = txtPassword.Text;
 
-            if (user == "Diego" && password == "1234")
+            using (var connection = ConexionDB.GetConnection())
             {
-                MainFrm mainfrm = new MainFrm();
-                mainfrm.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Usuario o contraseña incorrectos");
+                connection.Open();
+                string query = "SELECT Name FROM Users WHERE User = @user AND Password = @password";
+
+                using (var cmd = new SQLiteCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@user", user);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    var result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        string userName = result.ToString();
+                        MessageBox.Show($"Bienvenido, {userName}");
+
+                        // Open main form
+                        this.Hide();
+                        MainFrm mainfrm = new MainFrm();
+                        mainfrm.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
             }
         }
     }
---
+++ Helpers/ConexionDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace PuntoDeVenta.Helpers
+{
+    public static class ConexionDB
+    {
+        private static string ruteDB = Path.Combine(Application.StartupPath, @"DataBase\PointOfSale_db.db");
+
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection($"Data Source={ruteDB};Version=3;");
+        }
+    }
+}
